62cd492aac562235813bfba707417ec6b29b341b
AleksKoldashov <kapahdalli88@gmail.com>
2024-07-07 23:15:21 +0400

    v1.0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
-import React, { createContext, useEffect, } from 'react';
+import { useEffect, } from 'react';
 import './App.css'
 import MenuTopBar from './components/App/Menutopbar';
 import {Outlet, useNavigate} from 'react-router-dom';
 
-
-
-export const AppContext=createContext<any>({})
-
 function App() {
 
-
-
 const nav = useNavigate()
+
 useEffect(()=>{
   nav('/home')
 },[nav])
-
-
 
   return (
     <>
